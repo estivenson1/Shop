b3efe7d330c1b5ed4328fff7f68f1bc213a40230
EsOrVi_2 <csharp_eov@hotmail.com>
2019-03-15 12:47:07 -0500

    EO - API Products controller Tutorial 57 - .NET Core & Xamarin (2019-I)

---
+++ Shop.Web/Controllers/API/ProductsController.cs
@@ -0,0 +1,146 @@
+﻿namespace Shop.Web.Controllers.API
+{
+    using Data;
+    using Data.Entities;
+    using Helpers;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using System.Threading.Tasks;
+
+    [Route("api/[Controller]")]
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository productRepository;
+
+        //private readonly IUserHelper userHelper;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+            //this.userHelper = userHelper;
+        }
+
+        // GET: Products
+        public IActionResult GetProducts()
+        {
+            return Ok(this.productRepository.GetAll());
+        }
+
+        //// GET: Products/Details/5
+        //public async Task<IActionResult> Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var product = await this.productRepository.GetByIdAsync(id.Value);
+        //    if (product == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return View(product);
+        //}
+
+        //// GET: Products/Create
+        //public IActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        //// POST: Products/Create
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Create(Product product)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        // TODO: Pending to change to: this.User.Identity.Name
+        //        product.User = await this.userHelper.GetUserByEmailAsync("jzuluaga55@gmail.com");
+        //        await this.productRepository.CreateAsync(product);
+        //        return RedirectToAction(nameof(Index));
+        //    }
+
+        //    return View(product);
+        //}
+
+        //// GET: Products/Edit/5
+        //public async Task<IActionResult> Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var product = await this.productRepository.GetByIdAsync(id.Value);
+        //    if (product == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return View(product);
+        //}
+
+        //// POST: Products/Edit/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Edit(Product product)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        try
+        //        {
+        //            // TODO: Pending to change to: this.User.Identity.Name
+        //            product.User = await this.userHelper.GetUserByEmailAsync("jzuluaga55@gmail.com");
+        //            await this.productRepository.UpdateAsync(product);
+        //        }
+        //        catch (DbUpdateConcurrencyException)
+        //        {
+        //            if (!await this.productRepository.ExistAsync(product.Id))
+        //            {
+        //                return NotFound();
+        //            }
+        //            else
+        //            {
+        //                throw;
+        //            }
+        //        }
+        //        return RedirectToAction(nameof(Index));
+        //    }
+
+        //    return View(product);
+        //}
+
+        //// GET: Products/Delete/5
+        //public async Task<IActionResult> Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var product = await this.productRepository.GetByIdAsync(id.Value);
+        //    if (product == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return View(product);
+        //}
+
+        //// POST: Products/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> DeleteConfirmed(int id)
+        //{
+        //    var product = await this.productRepository.GetByIdAsync(id);
+        //    await this.productRepository.DeleteAsync(product);
+        //    return RedirectToAction(nameof(Index));
+        //}
+
+
+
+    }
+
+}
